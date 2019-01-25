33a63bf9796b2a6aae0535b9df8469a497d5e474
Jeremy Mapalad <jeremymapalad@gmail.com>
2019-01-25 02:34:16 -0800

    Separated temporary database to a .js file

--- src/js/App.js
+++ src/js/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import database from './data'
 
 const Header = (props) => (
 	<header>
@@ -56,52 +57,7 @@ class Preset extends React.Component {
 }
 
 class App extends React.Component {
-	state = {
-			"presets": [
-	    {
-	      "id": 1,
-	      "name": "Grand Scheme",
-	      "cards": [1, 2, 3]
-	    },
-	    {
-	      "id": 2,
-	      "name": "Deconstrution",
-	      "cards": [2, 3]
-	    },
-	    {
-	      "id": 3,
-	      "name": "Give and Take",
-	      "cards": [1, 3, 4]
-	    },
-	    {
-	      "id": 4,
-	      "name": "Reach For Tomorrow",
-	      "cards": [4]
-	    }
-	  ],
-	  "cards": [
-	    {
-	      "id": 1,
-	      "name": "Festival",
-	      "description": "+2 Actions, +1 Buy, +2 Gold"
-	    },
-	    {
-	      "id": 2,
-	      "name": "Witch",
-	      "description": "+2 Cards; Each other player gains a Curse."
-	    },
-	    {
-	      "id": 3,
-	      "name": "Militia",
-	      "description": "+2 Gold; Each other player discard down to 3 card in hand."
-	    },
-	    {
-	    	"id": 4,
-	      "name": "Laboratory",
-	      "description": "+2 Cards, +1 Action"
-	    }
-	  ]
-	}
+	state = database;
 
 	handleRemovePreset = (id) => {
     this.setState( prevState => {
@@ -115,7 +71,7 @@ class App extends React.Component {
 		return (
 			<div className="preset-builder">
        			<Header 
-       				title="Domininion Preset Builder" 
+       				title="Dominion Preset Builder" 
        				numberOfPresets={this.state.presets.length}
        			/>
 
---
+++ src/js/data.js
@@ -0,0 +1,48 @@
+const database = {
+		"presets": [
+    {
+      "id": 1,
+      "name": "Grand Scheme",
+      "cards": [1, 2, 3]
+    },
+    {
+      "id": 2,
+      "name": "Deconstrution",
+      "cards": [2, 3]
+    },
+    {
+      "id": 3,
+      "name": "Give and Take",
+      "cards": [1, 3, 4]
+    },
+    {
+      "id": 4,
+      "name": "Reach For Tomorrow",
+      "cards": [4]
+    }
+  ],
+  "cards": [
+    {
+      "id": 1,
+      "name": "Festival",
+      "description": "+2 Actions, +1 Buy, +2 Gold"
+    },
+    {
+      "id": 2,
+      "name": "Witch",
+      "description": "+2 Cards; Each other player gains a Curse."
+    },
+    {
+      "id": 3,
+      "name": "Militia",
+      "description": "+2 Gold; Each other player discard down to 3 card in hand."
+    },
+    {
+    	"id": 4,
+      "name": "Laboratory",
+      "description": "+2 Cards, +1 Action"
+    }
+  ]
+}
+
+export default database;
