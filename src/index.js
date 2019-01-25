e0351cb65e6cf13181b3c05c4854550f1889a68c
Jeremy Mapalad <jeremymapalad@gmail.com>
2019-01-24 20:27:27 -0800

    Initialize and base config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/main.scss'
+import App from './js/App.js';
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);
