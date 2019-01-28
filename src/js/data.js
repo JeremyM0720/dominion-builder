57c462e93ed45116566781f3bd0ceafc3f037692
Jeremy Mapalad <jeremymapalad@gmail.com>
2019-01-28 02:01:22 -0800

    Added sample cards and presets

--- src/js/data.js
+++ src/js/data.js
@@ -3,45 +3,80 @@ const database = {
     {
       "id": 1,
       "name": "Grand Scheme",
-      "cards": [1, 2, 3]
+      "cards": [1, 2, 3, 6, 8, 4]
     },
     {
       "id": 2,
       "name": "Deconstrution",
-      "cards": [2, 3]
+      "cards": [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     },
     {
       "id": 3,
       "name": "Give and Take",
-      "cards": [1, 2, 3, 4]
+      "cards": [1, 2]
     },
     {
       "id": 4,
       "name": "Reach For Tomorrow",
-      "cards": [4]
-    }
+      "cards": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    },
+    {
+      "id": 5,
+      "name": "Give and Take",
+      "cards": [7, 8, 3, 4]
+    },
   ],
   "cards": [
     {
       "id": 1,
-      "name": "Festival",
-      "description": "+2 Actions, +1 Buy, +2 Gold"
+      "name": "Cellar",
+      "description": "+1 Action. Discard any number of cards. +1 Card per card discarded."
     },
     {
       "id": 2,
-      "name": "Witch",
-      "description": "+2 Cards; Each other player gains a Curse."
+      "name": "Chapel",
+      "description": "Trash up to 4 cards from your hand."
     },
     {
       "id": 3,
-      "name": "Militia",
-      "description": "+2 Gold; Each other player discard down to 3 card in hand."
+      "name": "Moat",
+      "description": "+2 Cards. When another player plays an Attack card, you may reveal this from your hand. If you do, you are unaffected by that Attack."
     },
     {
     	"id": 4,
-      "name": "Laboratory",
-      "description": "+2 Cards, +1 Action"
-    }
+      "name": "Chancellor",
+      "description": "+2 Gold. You may immediately put your deck into your discard pile."
+    },
+    {
+      "id": 5,
+      "name": "Village",
+      "description": "+1 Card; +2 Actions."
+    },
+    {
+      "id": 6,
+      "name": "Woodcutter",
+      "description": "+1 Buy; +$2."
+    },
+    {
+      "id": 7,
+      "name": "Bureaucrat",
+      "description": "Gain a silver card; put it on top of your deck. Each other player reveals a Victory card from his hand and puts it on his deck (or reveals a hand with no Victory cards).",
+    },
+    {
+      "id": 8,
+      "name": "Feast",
+      "description": "Trash this card. Gain a card costing up to $5."
+    },
+    {
+      "id": 9,
+      "name": "Militia",
+      "description": "+2 Gold. Each other player discards down to 3 cards in his hand."
+    },
+    {
+      "id": 10,
+      "name": "Throne Room",
+      "description": "Choose an Action card in your hand. Play it twice."
+    },
   ]
 }
 
