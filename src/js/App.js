f96afe7ed82aace9ef0f9a1a8f72e1bad570b61a
Jeremy Mapalad <jeremymapalad@gmail.com>
2019-02-05 03:20:37 -0800

    Add PresetList as a component; Modify some method

--- src/js/App.js
+++ src/js/App.js
@@ -3,36 +3,14 @@ import database from './data';
 import Header from './Header';
 import Preset from './Preset';
 import AddPreset from './AddPreset';
+import PresetList from './PresetList';
 
 class App extends Component {
 	state = {
-						database,
-						selectedPreset: null
+						database
 					};
 
-	//Methods for Preset
-	handleRemovePreset = id => {
-    this.setState( prevState => {
-      return {
-      	database: {
-      		presets: prevState.database.presets.filter( preset => preset.id !== id ),
-      		cards: prevState.database.cards //tmp; can't figure out atm
-      	}
-      };
-    });
-  }
-
-  handleSelectPreset = id => {
-  	if(this.state.database.presets.find( preset => preset.id === id && preset.cards.length < 10)) {
-  		return false;
-  	}
-
-  	this.setState({
-  		selectedPreset: id
-  	});
-  }
-
-  handleAddPreset = name => {
+  addPreset = name => {
     this.setState({
       database: {
         presets: [
@@ -48,8 +26,18 @@ class App extends Component {
     });
   }
 
-  //Methods for Cards
-  handleDeleteCard = (presetId, cardId) => {
+  removePreset = id => {
+    this.setState( prevState => {
+      return {
+        database: {
+          presets: prevState.database.presets.filter( preset => preset.id !== id ),
+          cards: prevState.database.cards //tmp; can't figure out atm
+        }
+      };
+    });
+  }
+
+  deleteCard = (presetId, cardId) => {
   	this.setState( prevState => {
   		return {
   			database: {
@@ -74,34 +62,14 @@ class App extends Component {
        				numberOfPresets={this.state.database.presets.length}
        			/>
 
-       			<div className="preset-list">
-	       			{
-			          this.state.database.presets.map( preset => 
-			          	{
-			          		const presetCardsList = [];
-		          			preset.cards.map( cardNumber => {
-		          				presetCardsList.push(this.state.database.cards.find(card => card.id === cardNumber));
-		          			});
-
-			          		return (
-				            	<Preset 
-				             		id={preset.id}
-				             		key={preset.id.toString()}
-				             		name={preset.name} 
-				             		selectedPreset={this.state.selectedPreset}
-				             		selectPreset={this.handleSelectPreset}
-				             		removePreset={this.handleRemovePreset}
-				             		deleteCard={this.handleDeleteCard}
-				              	presetCardsList={presetCardsList}
-				           		/>
-				            );
-			          	}
-			          )
-			        }
-            </div>
+       			<PresetList 
+              database={this.state.database}
+              removePreset={this.removePreset}
+              deleteCard={this.deleteCard}
+            />
 
 		        <AddPreset 
-              addPreset={this.handleAddPreset}
+              addPreset={this.addPreset}
             /> 
 			</div> 
 		);
