806ff61fe51519827fed0e1056b569574ce68d10
Jeremy Mapalad <jeremymapalad@gmail.com>
2019-02-05 02:30:02 -0800

    Restructure for the new add preset form

--- src/js/App.js
+++ src/js/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 import database from './data';
 import Header from './Header';
 import Preset from './Preset';
+import AddPreset from './AddPreset';
 
 class App extends React.Component {
 	state = {
 						database,
-						newPresetName: null,
 						selectedPreset: null
 					};
 
@@ -22,12 +22,6 @@ class App extends React.Component {
     });
   }
 
-  handleNewPresetValue = name => { //tmp
-  	this.setState({
-  		newPresetName: name
-  	});
-  }
-
   handleSelectPreset = id => {
   	if(this.state.database.presets.find( preset => preset.id === id && preset.cards.length < 10)) {
   		return false;
@@ -39,22 +33,18 @@ class App extends React.Component {
   }
 
   handleAddPreset = name => {
-  	const newPreset = {
-							  			id: Math.floor((Math.random() * 10000) + 1),  
-								  		name: name,
-								  		cards: []
-							  		};
-  	this.setState( prevState => {
-      return {
-      	database: {
-      		presets: prevState.database.presets.concat(newPreset),
-      		cards: prevState.database.cards //tmp; can't figure out atm
-      	}
-      };
-    });
-
     this.setState({
-    	newPresetName: null
+      database: {
+        presets: [
+          ...this.state.database.presets,
+          {
+            id: this.state.database.presets.length + 1,  
+            name: name,
+            cards: []
+          }
+        ],
+        cards: this.state.database.cards
+      }
     });
   }
 
@@ -108,11 +98,11 @@ class App extends React.Component {
 			          	}
 			          )
 			        }
+            </div>
 
-			        <div className="preset preset-add">
-			        	<input type="text" id="newPresetName" onChange={e => this.handleNewPresetValue(e.target.value)}/><button className="button-action add" onClick={ () => this.handleAddPreset(this.state.newPresetName) }> + </button>
-			        </div>
-		        </div>
+		        <AddPreset 
+              addPreset={this.handleAddPreset}
+            /> 
 			</div> 
 		);
 	}
