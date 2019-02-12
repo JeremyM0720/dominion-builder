a63700821885a99ef3808b06486917988fac6e06
Jeremy Mapalad <jeremymapalad@gmail.com>
2019-02-12 01:28:10 -0800

    Convert to PureComponent, Destructured Props and Add PropTypes typecheck

--- src/js/Actions.js
+++ src/js/Actions.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
 
-class Actions extends Component {
+class Actions extends PureComponent {
 
 	handleSelectPreset = () => {
 		this.props.selectPreset(this.props.id);
@@ -11,17 +12,31 @@ class Actions extends Component {
   }
 
 	render() {
-		const cardCount = this.props.cardCount < 10 ? `Cards: ${this.props.cardCount}/10` : 'Complete';
-		const cardCountColor = this.props.cardCount < 10 ? '' : 'card-count-complete';
+		const { cardCount, id } = this.props;
+
+		let cardCounter;
+		let cardCountColor;
+
+		if ( cardCount < 10 ) {
+			cardCounter = `Cards: ${cardCount}/10`;
+		} else {
+			cardCounter = 'Complete';
+			cardCountColor = 'card-count-complete';
+		}
 
 		return (
 			<div className="actions">
-				<span className={`card-count ${cardCountColor}`}>{cardCount}</span>
+				<span className={`card-count ${cardCountColor}`}>{cardCounter}</span>
 				<button className="button-action add" onClick={ this.handleSelectPreset }> ✓ </button>
 				<button className="button-action delete" onClick={ this.handleRemovePreset }> ✖ </button>
 			</div>
 		);
 	}
+}
+
+Actions.proptypes = {
+	cardCount: PropTypes.number,
+	id: PropTypes.number
 }
 
 export default Actions;
